ddd746f4d12b3c2643b37cb64fcbcf16a677396f
Daniel Vargas García <dvg9383@gmail.com>
2024-05-04 21:07:45 +0200

    datambl

--- js/data.mjs
+++ js/data.mjs
@@ -2,13 +2,13 @@ export default [{
   brand: "Apple",
   model: "iPhone 15",
   variant: "Pro Max",
-  price: 1200,
+  price: 1200.00,
   id: "apple-iphone-15-pro-max"
 }, {
   brand: "Apple",
   model: "iPhone 15",
   variant: "Pro",
-  price: 983.5,
+  price: 983.50,
   id: "apple-iphone-15-pro"
 }, {
   brand: "Huawei",
@@ -22,4 +22,88 @@ export default [{
   variant: "m55",
   price: 350.45,
   id: "samsung-galaxy-m55"
+}, {
+  brand: "Xiaomi",
+  model: "Redmi",
+  variant: "Turbo 3",
+  price: 260.00,
+  id: "xiaomi-redmi-turbo3"
+}, {
+  brand: "Honor",
+  model: "200",
+  variant: "Lite",
+  price: 329.90,
+  id: "honor-200-lite"
+}, {
+  brand: "Honor",
+  model: "Magic 6",
+  variant: "Rsr Porsche Design",
+  price: 1275.00,
+  id: "honor-magic6-rsr-porsche-design"
+}, {
+  brand: "Honor",
+  model: "Magic v2",
+  variant: "Rsr Porsche Design",
+  price: 2699.00,
+  id: "honor-magic-v2-rsr-porsche-design"
+}, {
+  brand: "Samsung",
+  model: "Galaxy",
+  variant: "Z Fold 5",
+  price: 959.37,
+  id: "samsung-galaxy-z-fold5-5g"
+}, {
+  brand: "Samsung",
+  model: "Galaxy",
+  variant: "s23 Ultra 5g",
+  price: 970.00,
+  id: "samsung-galaxy-s23-ultra-5g"
+}, {
+  brand: "Apple",
+  model: "iPhone 14",
+  variant: "Pro Max",
+  price: 860.00,
+  id: "apple-iphone-14-pro-max-"
+}, {
+  brand: "Huawei",
+  model: "",
+  variant: "Pocket 2",
+  price: 962.00,
+  id: "huawei-pocket2"
+}, {
+  brand: "Huawei",
+  model: "",
+  variant: "P50 Pocket",
+  price: 566.00,
+  id: "huawei-p50-pocket"
+}, {
+  brand: "Xiaomi",
+  model: "14",
+  variant: "Ultra",
+  price: 1499.00,
+  id: "xiaomi-14-ultra-new"
+}, {
+  brand: "Oppo",
+  model: "A3",
+  variant: "Pro",
+  price: 416.46,
+  id: "oppo-a3-pro"
+}, {
+  brand: "Oppo",
+  model: "Reno",
+  variant: "11 F",
+  price: 349.00,
+  id: "oppo-reno11-f-5g"
+}, {
+  brand: "Oppo",
+  model: "Find X7",
+  variant: "Ultra",
+  price: 772.00,
+  id: "oppo-find-x7-ultra"
+}, {
+  brand: "Meizu",
+  model: "",
+  variant: "21 Pro",
+  price: 778.14,
+  id: "meizu-21-pro"
 }]
